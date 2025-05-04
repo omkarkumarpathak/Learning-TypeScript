45680b557893a6ff78230848ef3973ad3b6e8474
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2025-05-04 22:16:08 +0530

    Enum in TS

---
+++ SecondFile.js
@@ -0,0 +1,10 @@
+//enums
+var King;
+(function (King) {
+    King[King["First"] = 0] = "First";
+    King[King["Second"] = 1] = "Second";
+    King[King["Third"] = 2] = "Third";
+})(King || (King = {}));
+var variable = King.First;
+console.log(variable);
+console.log(1);
---
+++ SecondFile.ts
@@ -0,0 +1,30 @@
+/*
+
+//enums
+
+enum King{
+    First,
+    Second,
+    Third,
+}
+
+const variable=King.First;
+console.log(variable);
+
+//Note: By-default enum members start values=0 to 1 to...keep adding one
+
+
+
+//eg.2: 
+
+enum King{
+    First=45,
+    Second,
+    Third,
+}
+
+//Now, second & third have values 46, 47 etc
+
+
+*/
+
