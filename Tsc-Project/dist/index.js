2adbc14a88ab2872d2516c5f2ced6fead63518af
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2025-05-18 19:59:38 +0530

    Generic Functions

--- Tsc-Project/dist/index.js
+++ Tsc-Project/dist/index.js
@@ -17,32 +17,103 @@ class User{
 
 const object= new User("Om",23);
 
-*/
-//Note: Unlike js class, we've to pre-declare the variable 
+
+
+//Note: Unlike js class, we've to pre-declare the variable
 //and if declared, either define it in constructor or just give default
 //value at the declaration, but you can't just leave it undefined variable.
+
 // In js, you can clearly see the diff
+
+
 //But using access specifier we can make it more simpler
-class User {
-    constructor(email, name) {
-        this.email = email;
-        this.name = name;
-        this.email = email,
-            this.name = name;
+
+
+class User{
+
+    protected protected_var:number=0;
+
+    constructor(private email:string, public name:string){
+        this.email=email,
+        this.name=name;
     }
+
     //getter helps to fetch private data of class
-    get GetterBro() {
+    get GetterBro():string{
         return this.email;
     }
+
     //setter return no type
-    set setterBro(num1) {
-        this.email = num1;
+    set setterBro(num1:string){
+        this.email=num1;
     }
+
     //can create private methods as well
-    deleteBro() {
+    private deleteBro(){
         console.log("Private method");
     }
 }
-const om = new User("Om", "King");
-const value = om.GetterBro;
-om.setterBro = "hi";
+
+
+const om=new User("Om","King");
+
+
+const value:string=om.GetterBro;
+om.setterBro="hi";
+
+
+
+
+// Interface implementation
+//Interface creates like a structute, whose implementation must have
+
+//eg
+
+interface Story{
+    name:string,
+    email:string,
+    age:number
+}
+
+interface CreateBro{
+    createStory():void;
+}
+
+//Now, we we create class implementation, then they must have atleast these properties
+
+class User implements Story, CreateBro{
+    constructor(
+        public email:string,
+        public name:string,
+        public age:number,
+        public extra:string,
+    ){}
+
+    createStory(): void {
+        console.log("hi");
+    }
+}
+
+
+
+
+//Abstract: Has both structure + code
+
+abstract class User {
+    abstract name: string; // Declares that subclass must define this
+
+    coding(): void {
+        console.log("Method");
+    }
+}
+
+class SubUser extends User {
+    constructor(
+        public name: string,
+        public email: string
+    ) {
+        super(); // No arguments, since base class has no constructor
+    }
+}
+
+*/
---
+++ Tsc-Project/src/Generic.ts
@@ -0,0 +1,72 @@
+/*
+//Generics: we can give any type of data
+
+function genericFunction<Type>(val:Type):Type{
+    return val;
+}
+
+genericFunction(3);
+genericFunction("3");
+genericFunction(true);
+genericFunction([1,2,4,5]);
+genericFunction({om:"1",kar:"King"});
+
+
+
+
+
+//generic function: Array of any data type
+
+function genericFunction<Type>(val:Type[]):Type[]{
+    return val;
+}
+
+genericFunction([1,2,3,4]);
+genericFunction(["1","2","3","4"]);
+genericFunction([true,false]);
+
+
+
+//Or like can write Type[]==Array<Type>
+
+function genericFunction<Type>(val:Array<Type>):Array<Type>{
+    return val;
+}
+
+genericFunction([1,2,3,4]);
+genericFunction(["1","2","3","4"]);
+genericFunction([true,false]);
+
+
+//Or
+
+function genericFunction<T>(val:Array<T>):Array<T>{
+    return val;
+}
+
+genericFunction([1,2,3,4]);
+genericFunction(["1","2","3","4"]);
+genericFunction([true,false]);
+
+
+
+
+
+//Generic Arrow function
+
+// const genericFunction=()=>{               JS part
+//     return 5;
+// }
+
+const genericFunction=<T>(val:T[]):T=>{
+    return val[0];
+}
+    
+
+//Also in we can symbol as <T,>
+
+const genericFunction=<T,>(val:T[]):T=>{
+    return val[0];
+}
+
+*/
